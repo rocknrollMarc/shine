233a3259d1c9265a453cf78314b634d0545027e6
Marc Bluemner <marc.bluemner@gmail.com>
2016-08-10 14:08:49 +0200

    add sql schema for active record

--- db/migrate/20160809195744_add_email_constraint_to_users.rb
+++ db/migrate/20160809195744_add_email_constraint_to_users.rb
@@ -1,4 +1,19 @@
 class AddEmailConstraintToUsers < ActiveRecord::Migration
-  def change
-  end
+	def up
+		execute %{
+			ALTER TABLE
+				users
+			ADD CONSTRAINT
+				email_must_be_company_email
+			CHECK ( email ~* '^[^@]+@example\\.com' )
+		}
+	end
+	def down
+		execute %{
+			ALTER TABLE
+				users
+			DROP CONSTRAINT
+				email_must_be_company_email
+		}
+	end
 end
