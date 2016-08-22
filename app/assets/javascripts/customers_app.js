2bdf321801a20aa00cffa04077f5cc565a219524
Marc Bluemner <marc.bluemner@gmail.com>
2016-08-22 22:31:19 +0200

    reimplement pagination

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,18 +1,33 @@
-var app = angular.module("customers",[]); 
+var app = angular.module('customers',[]); 
 
-app.controller("CustomerSearchController", [
-          "$scope","$http",
-  function($scope , $http) {
+app.controller("CustomerSearchController", [ 
+          '$scope','$http',
+  function($scope , $http) {                         
+
+    var page = 0;
+
     $scope.customers = [];
-    $scope.search = function(searchTerm) {
-      $http.get("/customers.json",
-                { "params": { "keywords": searchTerm } }
-      ).then(function(response) {
-          $scope.customers = response.data;
-      },function(response) {
-          alert("There was a problem: " + response.status);
-        }
-      );
+    $scope.search = function(searchTerm) {   
+      $http.get("/customers.json",  
+                { "params": { "keywords": searchTerm, "page": page } }
+      ).success(
+        function(data,status,headers,config) { 
+          $scope.customers = data;
+      }).error(
+        function(data,status,headers,config) {
+          alert("There was a problem: " + status);
+        });
+    }
+
+    $scope.previousPage = function() {
+      if (page > 0) {
+        page = page - 1;
+        $scope.search($scope.keywords);
+      }
+    }
+    $scope.nextPage = function() {
+      page = page + 1;
+      $scope.search($scope.keywords);
     }
   }
 ]);
