3e1f68324d502ece31edae3fdbae11f9b7e3e1ea
Marc Bluemner <marc.bluemner@gmail.com>
2016-08-22 18:00:59 +0200

    name error

--- app/models/customer_search_term.rb
+++ app/models/customer_search_term.rb
@@ -18,7 +18,7 @@ class CustomerSearchTerm
 		@where_clause << case_insensitive_search(:first_name)
 		@where_args[:first_name] = starts_with(search_term)
 
-		@where_clause << " OR #{case_insensitive_search(last_name)}"
+		@where_clause << " OR #{case_insensitive_search(:last_name)}"
 		@where_args[:last_name] = starts_with(search_term)
 
 		@order = "last_name asc"
