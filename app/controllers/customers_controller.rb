d178169220177c0885a6391b3e3b6e19025b09c2
Marc Bluemner <marc.bluemner@gmail.com>
2016-08-22 20:18:59 +0200

    add offset(PAGE_SIZE)

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,6 +7,7 @@ class CustomersController < ApplicationController
 				customer_search_term.where_clause,
 				customer_search_term.where_args).
 			order(customer_search_term.order)
+			offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
 		else
 			@customers = []
 		end
